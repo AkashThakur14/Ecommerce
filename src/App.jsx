4e8d850b903a99f8c1e5f7f837241e410a2301df
AkashThakur14 <ak9988385532@gmail.com>
2025-05-07 18:33:56 +0530

    routs paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AppLayout } from "./components/layout/AppLayout";
 
+import {Home} from "../src/pages/Home"
+import {About} from "../src/pages/About"
+import {Contact} from "../src/pages/Contact"
+import {Products} from "../src/pages/Products"
 function App() {
+
+  const router = createBrowserRouter([{
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/products",
+        element: <Products />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  
+  }]);
 
   return (
     <>
-      <h1>E-comm</h1>
+     <RouterProvider router={router}> </RouterProvider>
     </>
   )
 }
