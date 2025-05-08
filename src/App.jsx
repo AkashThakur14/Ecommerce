035ee87b7267d0dcce8a3f76dc016f19109ee818
AkashThakur14 <ak9988385532@gmail.com>
2025-05-08 17:23:57 +0530

    common root css

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import {Home} from "../src/pages/Home"
 import {About} from "../src/pages/About"
 import {Contact} from "../src/pages/Contact"
 import {Products} from "../src/pages/Products"
+import { SingleProduct } from './pages/SingleProduct';
+import { Cart } from './pages/Cart';
+import { ErrorPage } from './pages/ErrorPage';
 function App() {
 
   const router = createBrowserRouter([{
@@ -26,9 +29,21 @@ function App() {
         element: <Products />
       },
       {
+        path: "/singleproduct/:id",
+        element: <SingleProduct />
+      },
+      {
+        path: "/cart",
+        element: <Cart />
+      },
+      {
         path: "contact",
         element: <Contact />
-      }
+      },
+      {
+        path: "*",
+        element: <ErrorPage />
+      },
     ]
   
   }]);
---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const SingleProduct = () => {
+  return (
+    <div>This is single page </div>
+  )
+}
+
