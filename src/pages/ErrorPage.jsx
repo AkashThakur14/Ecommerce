05c3ab9e81cd6c18a34e7a7875d1f8bda9957153
AkashThakur14 <ak9988385532@gmail.com>
2025-05-21 17:20:41 +0530

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 export const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+       <div className='error-wrapper pt-60 pb-60'>
+      <div className="container">
+       <div className="404-row">
+        <img src="./Images/404.png" alt="404-image" />
+        <h2>UH OH! You're lost</h2>
+
+       </div>
+        </div>
+        </div>
   )
 }
 
